0d63b8665f1abce905108812d392635a9c521453
Jiacong Xu <jiacong.xu@foxmail.com>
2017-06-27 22:47:49 +0800

    Restructured code packaging.

--- src/__main__.py
+++ src/__main__.py
@@ -9,19 +9,28 @@ import kivy
 kivy.require('1.9.0')
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.config import Config
+from kivy.clock import Clock
+from controllers.simulatorcontroller import SimulatorController
 
-class TransSim(App):
+class SimulatorApp(App):
     """
     This is the root app for the simulator.
     """
-
+    
     def build(self):
         """
         This returns a built widget for the app.
         """
-        return Label(text='Hello world')
+        Config.set('graphics', 'window_state', 'maximized')
+        
+        # We should now load the root controller. The root controller should
+        # handle the creation of UI. Then make the controller return the widget.
+        rootController = SimulatorController()
+        Clock.schedule_interval(rootController.update, 1 / 60)
+        
+        return rootController.view
 
 # Entry point.
 if __name__ == '__main__':
-    TransSim().run()
+    SimulatorApp().run()
---
+++ src/views/simulatorview.py
@@ -0,0 +1,11 @@
+#
+# Transmission Line Simulator
+# 
+# Author(s): Jiacong Xu
+# Created: May-28-2017
+#
+
+from kivy.uix.widget import Widget
+
+class SimulatorView(Widget):
+    pass
