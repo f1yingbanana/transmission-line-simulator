4b2c15373c821e99f25381cf4da13a0bc9021358
Jiacong Xu <jiacong.xu@foxmail.com>
2017-06-28 15:25:53 +0800

    Added basic layout.

--- src/views/cardview.py
+++ src/views/cardview.py
@@ -2,11 +2,10 @@
 # Transmission Line Simulator
 # 
 # Author(s): Jiacong Xu
-# Created: May-28-2017
+# Created: Jun-28-2017
 #
 
 from kivy.uix.widget import Widget
-from cardview import CardView
 
-class SimulatorView(Widget):
+class CardView(Widget):
     pass
